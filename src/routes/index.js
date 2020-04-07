42d6d6c7272dc972e3f7cdaaa0ceec59f4da7bbf
Milan Bancevic <m.bancevic@gmail.com>
2020-04-07 21:32:42 +0200

    zadatak 2 (axios )

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 import AddMovie from '../components/AddMovie';
-import Movies from '../components/Movie';
+import Movies from '../components/Movies';
 
 export const routes = [
 
   { path: '/movies', name: 'movies', component: Movies},
   { path: '/add-movie', name: 'add-movie', component: AddMovie},
-  { path: '*', redirect: '/movies'},
+  { path: '/', redirect: '/movies'},
 ];
