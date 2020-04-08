fd15da564c1048fddafa229211be2691c223a763
Milan Bancevic <m.bancevic@gmail.com>
2020-04-08 09:35:29 +0200

    zadatak 4(razclanjivanje komponente)

---
+++ src/components/MovieRow.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <table class="table">
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Director</th>
+        <th>url</th>
+        <th>Duration</th>
+        <th>Release date</th>
+        <th>Genre</th>
+      </tr>
+      <tr v-for="movie in movies" :key="movie.id">
+        <td>{{ movie.id }}</td>
+        <td>{{ movie.title }}</td>
+        <td>{{ movie.director }}</td>
+        <td>{{ movie.imageUrl }}</td>
+        <td>{{ movie.duration }}</td>
+        <td>{{ movie.releaseDate }}</td>
+        <td>{{ movie.genre }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'MovieRow',
+    props: ['movies']
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Movies.vue
+++ src/components/Movies.vue
@@ -1,32 +1,17 @@
 <template>
   <div>
-      <table class="table">
-      <tr>
-        <th>ID</th>
-        <th>Title</th>
-        <th>Director</th>
-        <th>url</th>
-        <th>Duration</th>
-        <th>Release date</th>
-        <th>Genre</th>
-      </tr>
-      <tr v-for="movie in movies" :key="movie.id">
-        <td>{{ movie.id }}</td>
-        <td>{{ movie.title }}</td>
-        <td>{{ movie.director }}</td>
-        <td>{{ movie.imageUrl }}</td>
-        <td>{{ movie.duration }}</td>
-        <td>{{ movie.releaseDate }}</td>
-        <td>{{ movie.genre }}</td>
-      </tr>
-    </table>
+    <movie-row :movies='movies'/>
   </div>
 </template>
 
 <script>
-
+import MovieRow from './MovieRow'
 import { mapGetters, mapActions } from 'vuex'
 export default {
+  name: 'Movies',
+  components: {
+    'movie-row': MovieRow
+  },
 
   computed: {
     ...mapGetters(['movies'])
